b9df76c492520be5c921c9c3c23b6734d6144329
adamk <adamkuczynski@gmail.com>
2022-05-30 22:01:38 +0200

    Update

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import stravalib.model
 from os.path import exists
 import os
 import ast
-import streamlit
 import folium
 import webbrowser
 import random
@@ -140,6 +139,7 @@ def get_all_activities():
 
     counter = 0
     polyLineList = []
+    distanceList = []
     for x in activityDF['id']:
         counter += 1
         print("Making the map for activity # %d" % counter)
@@ -147,8 +147,9 @@ def get_all_activities():
         streamDF = storeStream(typeList, activityStream)
         streamPoly = makePolyLine(streamDF)
         polyLineList.append(streamPoly)
+        distanceList.append(activityDF.loc[counter-1, 'distance'])
 
-    plotMap(polyLineList, 0)
+    plotMap(polyLineList, 0, distanceList)
 
     return 'All done!'
 
@@ -174,18 +175,20 @@ def makePolyLine(df):
     return latLongList
 
 
-def plotMap(activityPolyLine, num):
+def plotMap(activityPolyLine, num, distanceList):
+    activityMap = folium.Map(location=[activityPolyLine[0][0][0], activityPolyLine[0][0][1]], zoom_start=14,
+                             width='100%')
+    folium.TileLayer('cartodbpositron').add_to(activityMap)
+    folium.TileLayer('cartodbdark_matter').add_to(activityMap)
     if len(activityPolyLine) == 1:
-        activityMap = folium.Map(location=[activityPolyLine[0][0][0], activityPolyLine[0][0][1]], zoom_start=14, width='100%')
         folium.PolyLine(activityPolyLine).add_to(activityMap)
         activityMap.save(r'example' + str(num) + '.html')
         webbrowser.open(r'example' + str(num) + '.html')
     else:
-        activityMap = folium.Map(location=[activityPolyLine[0][0][0], activityPolyLine[0][0][1]], zoom_start=14, width='100%')
         baseColor = "#FF0000"
         counter = 1
         for poly in activityPolyLine:
-            folium.PolyLine(poly, color=baseColor).add_to(folium.FeatureGroup(name="Run #" + str(counter)).add_to(activityMap))
+            folium.PolyLine(poly, color=baseColor).add_to(folium.FeatureGroup(name="Run #" + str(counter) + ", Distance: " + str(distanceList[counter-1])).add_to(activityMap))
             baseColor = "#" + "%06x" % random.randint(0, 0x888888)
             counter += 1
 
